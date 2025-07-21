fc9e16a5d40070cc93d1bcd8e677b65c6de4c444
AJ Clarke <ajclarke81@gmail.com>
2025-07-21 11:23:26 -0400

    adding notes app files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italict'
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <p>
+        Note app, Department of Computer Science, Unifersity of Helsinski 2025
+      </p>
+    </div>
+  )
+}
+
+export default Footer
